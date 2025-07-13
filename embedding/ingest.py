7e4186cb1615fab77dc735d2056b5189ba93de55
cris <ripad3011@gmail.com>
2025-07-13 14:01:09 +0700

    update env path

--- embedding/ingest.py
+++ embedding/ingest.py
@@ -7,10 +7,10 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path='../.env')
 
 # Database setup
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/ecommerce_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
