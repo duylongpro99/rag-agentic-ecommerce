0032d3478ea0645fd67aa3f204186371fc70cace
cris <ripad3011@gmail.com>
2025-07-19 23:11:01 +0700

    web: vortex background

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -1,12 +1,15 @@
 import { caller } from '@/trpc/server';
 import { ConversationView } from '../views/conversation.view';
+import { Vortex } from '@/components/ui/vortex';
 
 export default async function Page() {
     void (await caller.auth.session());
 
     return (
         <main className="min-h-screen bg-background">
-            <ConversationView />
+            <Vortex backgroundColor="black" className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+                <ConversationView />
+            </Vortex>
         </main>
     );
 }
