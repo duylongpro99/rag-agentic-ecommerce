97cb6c15429cf43471f43f35ebe2c2720279be6a
cris <ripad3011@gmail.com>
2025-07-19 22:47:48 +0700

    web: navbar

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ interface Props {
     children: React.ReactNode;
 }
 
-const HomeLayout: React.FC<Props> = ({ children }) => {
+const AuthLayout: React.FC<Props> = ({ children }) => {
     return <div className="flex flex-col w-full min-h-screen gap-y-4 justify-center items-center">{children}</div>;
 };
 
-export default HomeLayout;
+export default AuthLayout;
--- web/src/app/(home)/layout.tsx
+++ web/src/app/(home)/layout.tsx
@@ -1,9 +1,16 @@
+import { HomeNavbar } from '@/components/home/navbar';
+
 interface Props {
     children: React.ReactNode;
 }
 
-const AuthLayout: React.FC<Props> = ({ children }) => {
-    return <div className="flex flex-col gap-y-4 justify-center items-center min-h-screen">{children}</div>;
+const HomeLayout: React.FC<Props> = ({ children }) => {
+    return (
+        <div className="flex flex-col gap-y-4 justify-center items-center min-h-screen">
+            <HomeNavbar />
+            {children}
+        </div>
+    );
 };
 
-export default AuthLayout;
+export default HomeLayout;
